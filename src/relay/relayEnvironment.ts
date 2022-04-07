75074ccfe3ed2c63b08ab14a01b8611e7562d4a6
dirty49374 <dirty49374@github.com>
2022-04-08 00:03:46 +0900

    WIP: fix AbortController issue

--- src/relay/relayEnvironment.ts
+++ src/relay/relayEnvironment.ts
@@ -11,39 +11,47 @@ import {
 import type { FetchFunction } from "relay-runtime";
 
 import fetchGraphQL from "./fetchGraphQL";
-import { createClient } from "graphql-sse";
-const sseClient = createClient({
-  url: "http://localhost:3000/api/graphql-sse",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  singleConnection: true,
-});
 
 let relayEnvironment: Environment;
 
-const fetchFn: FetchFunction = async (params, variables) => {
-  return fetchGraphQL(params.text!, variables);
-};
-
-const subscribeFn: SubscribeFunction = (operation, variables) => {
-  return Observable.create((sink) => {
-    sseClient.subscribe(
-      {
-        operationName: operation.name,
-        query: operation.text!,
-        variables,
-      },
-      sink as any
-    );
-  });
-};
-
 const createEnvironment = () => {
-  return new Environment({
-    network: Network.create(fetchFn, subscribeFn),
-    store: new Store(new RecordSource()),
-  });
+  const fetchFn: FetchFunction = async (params, variables) => {
+    return fetchGraphQL(params.text!, variables);
+  };
+
+  if (typeof window === "undefined") {
+    return new Environment({
+      network: Network.create(fetchFn),
+      store: new Store(new RecordSource()),
+    });
+  } else {
+    const { createClient } = require("graphql-sse");
+
+    const sseClient = createClient({
+      url: "http://localhost:3000/api/graphql-sse",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      singleConnection: true,
+    });
+    const subscribeFn: SubscribeFunction = (operation, variables) => {
+      return Observable.create((sink) => {
+        sseClient.subscribe(
+          {
+            operationName: operation.name,
+            query: operation.text!,
+            variables,
+          },
+          sink as any
+        );
+      });
+    };
+
+    return new Environment({
+      network: Network.create(fetchFn, subscribeFn),
+      store: new Store(new RecordSource()),
+    });
+  }
 };
 
 type InitialRecords = ConstructorParameters<typeof RecordSource>[number];
